61f1ed6c7c8fde290bd69aa6ed5136b9a921c1ff
Divyesh Modasiya <divyeshmodasiya@gmail.com>
2024-06-28 15:22:26 +0530

    second commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+     <div className="main w-full h-[500px] bg-black text-white flex justify-center items-center">
+            <h1 className='text-[50px]'>Click Here To Logout</h1>
+     </div>
+    </>
+  )
+}
+
+export default Home
